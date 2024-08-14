e020547dfe92eafa87890efe12b1fe58a5af039c
nhiennd <nhiennd@vnpt.vn>
2024-08-14 15:18:40 +0700

    first commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent {
+  selectedUser = DUMMY_USERS[0];
+
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+}
