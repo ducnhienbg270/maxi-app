03ec32c62994a4c4861ed0e7f77010b2cf691f7a
nhiennd <nhiennd@vnpt.vn>
2024-08-21 14:54:03 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { TasksComponent } from './tasks/tasks.component';
 import { TaskComponent } from './tasks/task/task.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { FormsModule } from '@angular/forms';
+import { CardComponent } from './shared/card/card.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { FormsModule } from '@angular/forms';
     UserComponent,
     TasksComponent,
     TaskComponent,
-    NewTaskComponent
+    NewTaskComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -14,7 +14,4 @@ export class TaskComponent {
     this.taskIdCompleted.emit(this.itemTask?.id);
   }
 
-  onCancelAddTask() {
-    this
-  }
 }
